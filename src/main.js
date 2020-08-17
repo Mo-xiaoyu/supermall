f51475bca3665c870901224d3b267db0f406fc06
zxd <18532673661@163.com>
2020-08-17 16:32:02 +0800

    使用图片懒加载

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueLazyLoad from 'vue-lazyload'
 
 import toast from 'components/common/toast/index.js'
 
 Vue.use(toast)
+Vue.use(VueLazyLoad, {
+  loading: require('./assets/img/common/placeholder.png')
+})
 
 Vue.config.productionTip = false
 //事件总线
