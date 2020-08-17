0a9f749655a32fd365876414bdd87a92c5932b43
zxd <18532673661@163.com>
2020-08-17 15:57:03 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import toast from 'components/common/toast/index.js'
+
+Vue.use(toast)
+
 Vue.config.productionTip = false
 //事件总线
 Vue.prototype.$bus = new Vue()
