81245f2ef74a471912056446e13cdad8535736bb
zxd <18532673661@163.com>
2020-09-13 10:37:42 +0800

    调整

--- postcss.config.js
+++ postcss.config.js
@@ -9,7 +9,7 @@ module.exports = {
       selectorBlankList: ['ignore', 'tab-bar'], //指定不需要转换的类
       minPixelValue: 1, //小于或等于‘1px’不转换为视口单位
       mediaQuery: false,//允许在媒体查询中转换为‘px’
-      exclude:[/TabBar/]  //不需要转化的组件文件名正则，必须是正则表达式
+      exclude:[/TabBar/, /CartBottomBar/]  //不需要转化的组件文件名正则，必须是正则表达式
     }
   }
 }
--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,16 @@ const path = require("path")
 const resolve = (dir) => path.join(__dirname, dir)
 
 module.exports = {
+  devServer: {
+  	overlay: { //让浏览器 overlay 同时显示警告和错误
+  		warnings: true,
+  		errors: true
+  	},
+  	host: "localhost",
+  	port: 8010,
+  	open: false,
+  	hotOnly: true
+  },
   chainWebpack: config => {
     //配置别名
   	config.resolve.alias
